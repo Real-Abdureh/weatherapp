cd13c981ebeecf78bbc7e4db3cc0e6c0fb7917b0
Real-Abdureh <abdouliabbas75@gmail.com>
2022-08-31 13:41:35 -0700

    changes

--- weatherprjct/asgi.py
+++ weatherprjct/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for weatherprj project.
+ASGI config for weatherprjct project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherprj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherprjct.settings')
 
 application = get_asgi_application()
